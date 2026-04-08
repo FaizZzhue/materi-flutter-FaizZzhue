f6ed0bae1890b374ca8f184ac917c329b9ff406a
FaizZzhue <fynyourbaeid18@gmail.com>
2026-04-08 20:27:26 +0700

    chore: initial commit

---
+++ daftar_belanja/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:daftar_belanja/firebase_options.dart';
+import 'package:daftar_belanja/screens/shopping_list_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
